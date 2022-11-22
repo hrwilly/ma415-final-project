2f95c624bbdfa84fb3dfc0899f308e149962a2b3
Maysen Pagan <mppagan@bu.edu>
2022-11-21 21:33:38 -0500

    final baseball and payroll edits for DATA page

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -27,6 +27,38 @@ baseball <- baseball %>% mutate(score_diff=h_score-v_score)
 save(baseball, file = "dataset/baseball.RData")
 
 
+#Winning Percentage RData file
+
+#win/loss column (1 = win, 0 = loss or tie)
+h_win_loss <- function(h, v) {
+  ifelse(h > v, 1, 0)
+}
+(baseball <- baseball %>% mutate(h_win_loss = h_win_loss(h_score, v_score)))
+
+#total games won
+home_wins <- baseball %>% group_by(h_name, year) %>% summarize(h_wins = sum(h_win_loss == 1))
+away_wins <- baseball %>% group_by(v_name, year) %>% summarize(a_wins = sum(h_win_loss == 0))
+team_year_wins <- home_wins %>% add_column(a_wins = away_wins$a_wins) %>% 
+  mutate(games_won = h_wins + a_wins)
+#total games played
+home_games <- baseball %>% group_by(h_name, year) %>% summarize(home_games = n())
+away_games <- baseball %>% group_by(v_name, year) %>% summarize(away_games = n())
+win_perc <- home_games %>% add_column(away_games = away_games$away_games) %>% 
+  mutate(games_played = home_games + away_games) %>% 
+  add_column(games_won = team_year_wins$games_won) %>% 
+  mutate(win_perc = games_won/games_played)
+
+save(win_perc, file = "dataset/win_perc.RData")
+
+
+#MLB Team Payrolls Dataset
+mlb_payroll <- as_tibble(read_csv(here::here("dataset", "mlb_payroll.csv"), show_col_types = FALSE))
+
+mlb_payroll$year <- as.character(mlb_payroll$year)
+
+save(mlb_payroll, file = "dataset/mlb_payroll.RData")
+
+
 #Stadium Locations Dataset
 save(MLBstadiums, file = "dataset/MLBstadiums.RData")
      
