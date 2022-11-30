e73c50a196689672a3267bc5aedd5d3a63e9b36d
Hannah Willy <hrwilly@bu.edu>
2022-11-30 09:46:31 -0500

    year to numeric

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -11,7 +11,7 @@ years<-c(2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2
 baseball <- baseball %>% separate(date,into=c("year","month_day"),sep=4) %>% 
                          separate(month_day,into=c("month","day"),sep=2) %>% 
                          filter(year %in% years) %>% 
-                         mutate(score_diff=h_score-v_score)
+                         mutate(score_diff=h_score-v_score,year=as.numeric(year))
 
 save(baseball, file = "dataset/baseball.RData")
 
@@ -44,8 +44,6 @@ save(win_perc, file = "dataset/win_perc.RData")
 
 #MLB Team Payrolls Dataset
 mlb_payroll <- as_tibble(read_csv(here::here("dataset", "mlb_payroll.csv"), show_col_types = FALSE))
-
-mlb_payroll$year <- as.character(mlb_payroll$year)
 
 save(mlb_payroll, file = "dataset/mlb_payroll.RData")
 
