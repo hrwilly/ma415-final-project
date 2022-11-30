7427b9a5a52e2dc8b09e7e19103a34d8f984dccb
Hannah Willy <hrwilly@bu.edu>
2022-11-30 09:38:43 -0500

    year to chr

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -11,7 +11,7 @@ years<-c(2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2
 baseball <- baseball %>% separate(date,into=c("year","month_day"),sep=4) %>% 
                          separate(month_day,into=c("month","day"),sep=2) %>% 
                          filter(year %in% years) %>% 
-                         mutate(score_diff=h_score-v_score,year=as.numeric(year))
+                         mutate(score_diff=h_score-v_score))
 
 save(baseball, file = "dataset/baseball.RData")
 
