ca9defc2756eaaf9415bbce02152357e1dd0097d
Hanah Holbert <hmholber@bu.edu>
2022-12-07 17:33:08 -0500

    more shiny app

--- shiny_app/stadium_map_2/app.R
+++ shiny_app/stadium_map_2/app.R
@@ -1,58 +1,3 @@
-# #
-# # This is a Shiny web application. You can run the application by clicking
-# # the 'Run App' button above.
-# #
-# # Find out more about building applications with Shiny here:
-# #
-# #    http://shiny.rstudio.com/
-# #
-# 
-# library(shiny)
-# 
-# # Define UI for application that draws a histogram
-# ui <- fluidPage(
-# 
-#     # Application title
-#     titlePanel("Old Faithful Geyser Data"),
-# 
-#     # Sidebar with a slider input for number of bins 
-#     sidebarLayout(
-#         sidebarPanel(
-#             sliderInput("bins",
-#                         "Number of bins:",
-#                         min = 1,
-#                         max = 50,
-#                         value = 30)
-#         ),
-# 
-#         # Show a plot of the generated distribution
-#         mainPanel(
-#            plotOutput("distPlot")
-#         )
-#     )
-# )
-# 
-# # Define server logic required to draw a histogram
-# server <- function(input, output) {
-# 
-#     output$distPlot <- renderPlot({
-#         # generate bins based on input$bins from ui.R
-#         x    <- faithful[, 2]
-#         bins <- seq(min(x), max(x), length.out = input$bins + 1)
-# 
-#         # draw the histogram with the specified number of bins
-#         hist(x, breaks = bins, col = 'darkgray', border = 'white',
-#              xlab = 'Waiting time to next eruption (in mins)',
-#              main = 'Histogram of waiting times')
-#     })
-# }
-# 
-# # Run the application 
-# shinyApp(ui = ui, server = server)
-# 
-# 
-# --------------------------------------------------------------------
-
 library(shiny)
 library(tidyverse)
 library(rvest)
@@ -61,17 +6,17 @@ library(leaflet)
 library(dplyr)
 library(readr)
 
-
-# ui <- ui <- fluidPage(
-#   leafletOutput("mymap"),
-#   p()
-# )
+load(here::here("dataset", "shiny_wins.RData"))
+load(here::here("dataset", "MLBstadiums.RData"))
 
 ui <- fluidPage(
   fluidRow(column(width=12, leafletOutput("mymap"))),
   fluidRow(
     column(width=4, 
-           selectizeInput(inputId = "yearSelected",label = "Year", choices = win_perc$year),
+           selectizeInput(inputId = "yearSelected",
+                            label = "Year", 
+                          choices = win_perc$year, 
+                         selected = win_perc$year[3]),
            uiOutput(win_perc$year))
   )
 )
@@ -88,8 +33,8 @@ server <- function(input, output, session){
   output$mymap <- renderLeaflet({
     #df <- data()
     
-    winning <- left_join( MLBstadiums, win_perc,by = c("Abbreviation" = "h_name")) %>% filter(year %in% uiOutput )
-    
+    winning <- left_join( MLBstadiums, win_perc,by = c("Abbreviation" = "h_name")) %>% 
+      filter(year %in% input$yearSelected)
     
     m <- leaflet(winning) %>% 
       setView(lng = -98.5795, lat = 39.8283, zoom = 5) %>% 
@@ -97,7 +42,7 @@ server <- function(input, output, session){
       addAwesomeMarkers(~Longitude, ~Latitude, 
                         icon = icons, 
                         label = ~as.character(Venue), 
-                        popup = ~as.character(winning)) %>% 
+                        popup = ~as.character(winning$win_perc)) %>% 
       addLegend(position = 'bottomleft', 
                 colors = makeColorsandNames[,2],
                 labels = makeColorsandNames[,1],
